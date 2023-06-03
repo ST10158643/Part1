72d27ea980fae31fe89e0ac5bb9a4a66aea128cb
ST10158643 <ST10158643@vcconnect.edu.za>
2023-06-03 23:56:55 +0200

    Addition of foodGroup, calories , string calories variables for Ingredient Class.
    Updated DisplayIngredient Method that pass an int to indicate ingredient number.

--- Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1/Classes/Ingredient.cs
+++ Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1/Classes/Ingredient.cs
@@ -8,11 +8,28 @@ namespace Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1.Classes
 {
     public class Ingredient
     {
-
         /// <summary>
         /// Holds the ingredients number 
         /// </summary>
-        public int Number { get; set; } = 0;
+       // public int Number { get; set; } = 0;
+
+        ///NEW VARIBLES
+
+        /// <summary>
+        /// Holds the calories for the ingredient
+        /// </summary>
+        public double Calories { get; set; } = 0.0;
+        //public double Calories { get; private set; } = 0.0;
+
+        // <summary>
+        /// Holds the string word quantity of the ingredients
+        /// </summary>
+        public string strCalories { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Holds the Food Group for the ingredient
+        /// </summary>
+        public string FoodGroup { get; set; } = string.Empty;
 
         /// <summary>
         /// Holds the name of the ingredient
@@ -28,10 +45,12 @@ namespace Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1.Classes
         /// Holds the quantity for the ingredient
         /// </summary>
         public double Quantity { get; set; } = 0.0;
+
         // <summary>
         /// Holds the string word quantity of the ingredients
         /// </summary>
         public string strQuantity { get; set; } = string.Empty;
+
         //---------------------------------------------------------------------------------------------------------//
         /// <summary>
         /// Default Constructor
@@ -41,14 +60,17 @@ namespace Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1.Classes
 
 
         }
+     
         //---------------------------------------------------------------------------------------------------------//
         /// <summary>
         /// Method to Print Array Objects
         /// </summary>
-        public void DisplayIngredients()
+        public void DisplayIngredients(int i)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(String.Format("\n{0,-7} {1,-7} {2,-5} {3,-5} {4,-5}", "", "-*- " + this.strQuantity, this.UnitofM, "of " + this.Name, " "));
+            Console.WriteLine($"\n{i.ToString().PadLeft(10)}.  {this.strQuantity.ToUpper()}{this.UnitofM} of {this.Name}");
+            Console.WriteLine($"\n{"".PadLeft(15)}[Calories: {this.Calories}][Food Group: {this.FoodGroup}]");
+
         }
     }
 }//__---____---____---____---____---____---____---__.ooo END OF FILE ooo.__---____---____---____---____---____---____---__\\
