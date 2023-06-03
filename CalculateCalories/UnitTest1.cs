b7fade2811a065b73acaee8ecf83744b5ff212ea
ST10158643 <ST10158643@vcconnect.edu.za>
2023-06-04 00:44:56 +0200

    Addition of Unit Test Class - tests CalculateCalories Method of Recipe Classs -method used to calculate the recipe total calories

---
+++ CalculateCalories/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Hannah_Ruth_Michaelson__ST10158643_PROG_6221_Part_1.Classes;
+using System.Collections.Generic;
+
+namespace CalculateCalories
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void CalcuCalories_Test()
+        {
+          
+            List<double> doubleList = new List<double> {100,100,200,100};
+          
+            Recipe obj = new Recipe();
+            //may have to add a para that is for the total Cal 
+            double totalCal = obj.CalculateCalories(doubleList);
+            double resultTotalCal = 500;
+            Assert.AreEqual(resultTotalCal, totalCal);
+            
+            
+        }
+    }
+}
